f3613d37954bee8949976a81f786388c845062a1
Simon Atkins <simonatk@gmail.com>
2018-08-12 10:31:18 +0100

    Add 'Add to Coop' button to directive birdAddToCoop

--- js/directives/birdAddToCoop.js
+++ js/directives/birdAddToCoop.js
@@ -1,9 +1,23 @@
 app.directive('birdAddToCoop', function() {
   return {
     restrict: 'E',
-    scope: {
-      info: "="
-    },
-    templateUrl: 'js/directives/birdAddToCoop.html'
+    scope: {},
+    templateUrl: 'js/directives/birdAddToCoop.html',
+
+    link: function(scope, element, attrs) {
+      scope.buttonText = "Add to Coop",
+      scope.added = false,
+
+      scope.addToCoop = function() {
+        element.toggleClass('btn-active')
+        if(scope.added) {
+          scope.buttonText = "Add to Coop";
+          scope.added = false;
+        } else {
+          scope.buttonText = "Remove from Coop";
+          scope.added = true;
+        }
+      }
+    }
   };
 })
